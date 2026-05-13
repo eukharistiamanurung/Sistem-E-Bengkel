879fbbee18bba93da5d9e4b48530e1da0b781cad
eukharistiamanurung51@gmail.com <eukharistiamanurung>
2026-05-13 13:21:57 +0700

    Membuat model kendaraan dan menerapkan proteksi Mass Assigment

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    //
+    protected $table = 'kendaraans';
+
+    protected $fillable = [
+        'plat_nomor',
+        'nama_pemilik',
+        'merk_kendaraan',
+        'keluhan'
+    ];
 }
